78ca8de5c29b8a7f5afa7dbb9861c16cf0d9ba44
Steffen Gröner <steffen@groener.email>
2024-12-09 16:09:14 +0100

    aufgabe 4 in arbeit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(eingabe, element) {
+
+    for(i = 1; i < arguments.length; i++) {
+
+
+        console.log(eingabe);
+        console.log(arguments[i]);
+        console.log(eingabe.includes(arguments[i]));
+
+        while (eingabe.includes(arguments[i] == true)) {
+            console.log("schleife läuft");
+            eingabe.splice(arguments[i]-1,1);
+        }
+    } 
+
+
+    return eingabe;
 };
+
+console.log(removeFromArray([1,2,3,4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
