0b9edbc9fb31979f95f0cf6d5ef498085b24dd25
Steffen Gröner <steffen@groener.email>
2024-12-09 11:26:48 +0100

    aufgabe 1-3 erledigt

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let longstring = "";
+
+    if(num < 0) {
+        return "ERROR";
+    }
+
+
+    for(let i = 0; i < num; i++) {
+        longstring += string;
+    }
+    return longstring;
+
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(input) {
+
+let einzeln = input.split("") ;
+let backwards = "" ;
+
+for(let i = einzeln.length -1; i >= 0; i--) {
+
+    backwards += einzeln[i];
+
+}
+
+return backwards;
 
 };
+
+console.log(reverseString("test"));
 
 // Do not edit below this line
 module.exports = reverseString;
