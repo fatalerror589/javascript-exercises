02493b930cf2fb230a2ee469907083492fa85aa7
Steffen Gröner <steffen@groener.email>
2025-01-04 13:46:16 +0100

    findTheOldest erledigt

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(person) {
 
+
+const withAge = person.toSorted((a, b) => (getAge(a) - getAge(b)));
+
+
+console.log(withAge);
+return withAge[withAge.length - 1];
 };
+
+function getAge(person){
+
+    person.yearOfDeath ??= new Date().getFullYear();
+    console.log(person.yearOfDeath - person.yearOfBirth);
+    return person.yearOfDeath - person.yearOfBirth;
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+
+//28 carly
+//49 ray
+//29 jane
